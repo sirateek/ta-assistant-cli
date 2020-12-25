881510f7f418f8fc898e1ef8eb51ca3639a92618
vitvara <gridxlgm@gmail.com>
2020-12-25 16:57:24 +0700

    :Delete: core/job **pep8

--- core/job/job.py
+++
@@ -1,7 +0,0 @@
-class Job:
-
-    def __init__(self, file_name: str) -> None:
-        self.file_name = file_name
-        self.state = "not checked"
-        self.job_var = {}
-        self.score = {}
