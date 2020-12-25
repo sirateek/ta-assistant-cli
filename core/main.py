323067f0c2554e4d7408c3abe3324141ef3ac635
Siratee K <sirateek@gmail.com>
2020-12-25 14:15:23 +0700

    :New: Add start method to core

--- core/main.py
+++ core/main.py
@@ -1,6 +1,10 @@
 from display_module.display_module import TaAssisDisplay
+import os
 
 
 class TaAssistant(TaAssisDisplay):
     def __init__(self):
+        pass
+
+    def start(self):
         pass
