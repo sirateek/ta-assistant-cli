6d673c3162a1a2bc4f8599141bd5281b92d3d9c2
vitvara <gridxlgm@gmail.com>
2020-12-27 23:33:46 +0700

    :Remove: remove unused docstring

--- core/job_list/job_list.py
+++ core/job_list/job_list.py
@@ -14,14 +14,7 @@ class JobList:
         """
         Args:
             path_to_run (str): path to run
-            job_draft (str): job_draft.json
-
-        Attributes:
-            self.__work : 
-            self.__student_data : 
-            self.__key_list : this valuable hold key that draft.json [zip_file_draft] provided
-            self.__split_list : keep string that are used for classification
-            self.__rated_student :              
+            job_draft (str): job_draft.json              
         """
         self.__path_to_run = path_to_run
         self.__job_draft = job_draft
